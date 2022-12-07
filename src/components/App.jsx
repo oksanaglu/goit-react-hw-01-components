8729157abbea1c90cb22aeafb4aebd3a1e761702
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-12-07 23:13:54 +0200

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>App</div>;
 };
