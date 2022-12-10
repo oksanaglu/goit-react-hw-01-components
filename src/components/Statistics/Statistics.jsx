056251584128a224655138ecaa57c192a7f9179a
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-12-10 20:46:29 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -22,9 +22,9 @@ Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
     })
   ),
 };
